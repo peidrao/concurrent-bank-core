12f2d99a75a26fc8f4a2f2f7f6232946fd024dca
Pedro Fonseca <contatopedrorn@gmail.com>
2026-04-02 10:48:07 -0300

    feat(project): add initial Mix project configuration

---
+++ mix.exs
@@ -0,0 +1,28 @@
+defmodule BankCrud.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :bank_crud,
+      version: "0.1.0",
+      elixir: "~> 1.16",
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  def application do
+    [
+      extra_applications: [:logger, :runtime_tools],
+      mod: {BankCrud.Application, []}
+    ]
+  end
+
+  defp deps do
+    [
+      {:ecto_sql, "~> 3.12"},
+      {:postgrex, ">= 0.0.0"},
+      {:jason, "~> 1.4"}
+    ]
+  end
+end
