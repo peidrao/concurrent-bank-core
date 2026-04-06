bbb4066191e8dcf685bfa7f235686da54ff6230a
Pedro Fonseca <contatopedrorn@gmail.com>
2026-04-06 11:52:17 -0300

    feat(auth): add AuthController for user management

---
+++ lib/bank_crud_web/controllers/auth_controller.ex
@@ -0,0 +1,29 @@
+defmodule BankCrudWeb.AuthController do
+  use BankCrudWeb, :controller
+
+  alias BankCrud.UserManager
+
+  # action_fallback(BankCrud.FallbackController)
+
+  # def login(conn, params) do
+  #   with {:ok, user} <- UserManager.authenticate_user(params) do
+  #     conn
+  #     |> put_status(:ok)
+  #   end
+  # end
+
+  def create(conn, params) do
+    with {:ok, user} <- UserManager.create_user(params) do
+      conn
+      |> put_status(:created)
+      |> json(%{data: user_payload(user)})
+    end
+  end
+
+  defp user_payload(user) do
+    %{
+      id: user.id,
+      username: user.username
+    }
+  end
+end
