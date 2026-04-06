87d9db937e2dc7a33486491daefd162945241929
Pedro Fonseca <contatopedrorn@gmail.com>
2026-04-06 10:47:42 -0300

    chore(user): reorder primary and foreign key definitions

--- lib/bank_crud/user_manager/user.ex
+++ lib/bank_crud/user_manager/user.ex
@@ -2,6 +2,9 @@ defmodule BankCrud.UserManager.User do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @foreign_key_type :binary_id
+
   schema "users" do
     field(:username, :string)
     field(:password, :string)
