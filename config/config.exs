03d82a9965df610f87476a0ccc0643bd93d6c79f
Pedro Fonseca <contatopedrorn@gmail.com>
2026-04-06 20:23:08 -0300

    feat(endpoint): add error rendering configuration

--- config/config.exs
+++ config/config.exs
@@ -3,7 +3,9 @@ import Config
 config :bank_crud,
   ecto_repos: [BankCrud.Repo]
 
-config :bank_crud, BankCrudWeb.Endpoint, url: [host: "localhost"]
+config :bank_crud, BankCrudWeb.Endpoint,
+  url: [host: "localhost"],
+  render_errors: [formats: [json: BankCrudWeb.ErrorJSON], layout: false]
 
 config :bank_crud, BankCrud.Repo,
   migration_primary_key: [name: :id, type: :binary_id],
